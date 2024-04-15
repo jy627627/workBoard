982e091c430de8cc00939ceaa37553365cea6222
BRITNEY.姜二丫 <9908104+jy627627@user.noreply.gitee.com>
2024-04-15 17:39:35 +0800

    button-done&search-toDo

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import { cn } from "@/lib/utils";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
+import { Hint } from "@/components/hint";
 
 
 interface ItemProps {
@@ -33,18 +34,25 @@ export const Item = ({
             aspect-square
             relative
         ">
-            <Image
-                fill
-                alt={ name }
-                src={ imageUrl }
-                onClick={ onClick }
-                className={
-                    cn(
-                        'rounded-md cursor-pointer opacity-75 hover:opacity-100 transition',
-                        isActive && 'opacity-100'
-                    )
-                }
-            />
+            <Hint
+                label={ name }
+                side="right"
+                align="start"
+                sideOffset={ 16 }
+            >
+                <Image
+                    fill
+                    alt={ name }
+                    src={ imageUrl }
+                    onClick={ onClick }
+                    className={
+                        cn(
+                            'rounded-md cursor-pointer opacity-75 hover:opacity-100 transition',
+                            isActive && 'opacity-100'
+                        )
+                    }
+                />
+            </Hint>
         </div>
     )
 }
