4cc60f77170d40ef6f8170ffa6cabc0b49487506
BRITNEY.姜二丫 <9908104+jy627627@user.noreply.gitee.com>
2024-04-18 17:51:01 +0800

    inviteButton-todo

---
+++ app/(dashboard)/_components/invite-button.tsx
@@ -0,0 +1,8 @@
+import { Plus } from 'lucide-react'
+import { OrganizationProfile } from '@clerk/nextjs'
+import {
+    Dialog,
+    DialogContent,
+    DialogTrigger
+} from '@/components/ui/dialog'
+
